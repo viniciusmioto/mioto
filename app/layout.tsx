a312b90362e66886e7242f42456ae8575c4bebb5
viniciusmioto <vsmioto@gmail.com>
2026-06-21 01:52:37 -0400

    fix: suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning>
         <Header />
         <main>{children}</main>
         <Footer />
